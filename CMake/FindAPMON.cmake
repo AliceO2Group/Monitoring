f2757fae18b0fc0e1ea2648048b672254831b59c
vascobarroso <vasco.barroso@gmail.com>
2015-06-24 17:21:48 +0200

    improved comments

--- CMake/FindAPMON.cmake
+++ CMake/FindAPMON.cmake
@@ -1,11 +1,11 @@
-# - Try to find ACT include dirs and libraries
+# - Try to find ApMon include dirs and libraries
 # Author: Barthelemy von Haller
 #
 # This script will set the following variables:
-#  ACT_FOUND - System has ACT
-#  ACT_INCLUDE_DIRS - The ACT include directories
-#  ACT_LIBRARIES - The libraries needed to use ACT
-#  ACT_DEFINITIONS - Compiler switches required for using ACT
+#  APMON_FOUND - System has ApMon
+#  APMON_INCLUDE_DIRS - The ApMon include directories
+#  APMON_LIBRARIES - The libraries needed to use ApMon
+#  APMON_DEFINITIONS - Compiler switches required for using ApMon
 
 # Init
 include(FindPackageHandleStandardArgs)
@@ -19,7 +19,7 @@ set(APMON_INCLUDE_DIRS ${APMON_INCLUDE_DIR} )
 find_library(APMON_LIBRARY NAMES apmoncpp HINTS /usr/local )
 set(APMON_LIBRARIES ${APMON_LIBRARY} )
 
-# handle the QUIETLY and REQUIRED arguments and set ACT_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set APMON_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(APMON  DEFAULT_MSG
                                   APMON_LIBRARY APMON_INCLUDE_DIR)
