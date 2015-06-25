fe477e8adfcd8421acc5b22f9fb96b640721f5b6
Barthelemy <barthelemy.von.haller@cern.ch>
2015-06-25 14:14:15 +0200

    better error message if apmon missing

--- CMake/FindAPMON.cmake
+++ CMake/FindAPMON.cmake
@@ -13,15 +13,15 @@ include(FindPackageHandleStandardArgs)
 # find includes
 find_path(APMON_INCLUDE_DIR ApMon.h
           PATHS /usr/local/include)
-set(APMON_INCLUDE_DIRS ${APMON_INCLUDE_DIR} )
+set(APMON_INCLUDE_DIRS ${APMON_INCLUDE_DIR})
 
 # find library
-find_library(APMON_LIBRARY NAMES apmoncpp HINTS /usr/local )
-set(APMON_LIBRARIES ${APMON_LIBRARY} )
+find_library(APMON_LIBRARY NAMES apmoncpp HINTS /usr/local)
+set(APMON_LIBRARIES ${APMON_LIBRARY})
 
 # handle the QUIETLY and REQUIRED arguments and set APMON_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(APMON  DEFAULT_MSG
+find_package_handle_standard_args(APMON  "ApMon could not be found. Install package ApMon_cpp."
                                   APMON_LIBRARY APMON_INCLUDE_DIR)
 
-mark_as_advanced(APMON_INCLUDE_DIR APMON_LIBRARY )
+mark_as_advanced(APMON_INCLUDE_DIR APMON_LIBRARY)
