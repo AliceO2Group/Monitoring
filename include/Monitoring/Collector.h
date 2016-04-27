74888abe1af940c226bc672de1435743ae3a6e97
awegrzyn <adam.wegrzynek@cern.ch>
2016-04-27 20:51:44 +0200

    Monitoring : cmake : including ApMon files only when lib found

--- include/Monitoring/Collector.h
+++ include/Monitoring/Collector.h
@@ -14,45 +14,46 @@ namespace AliceO2 {
 namespace Monitoring {
 /// Core features of ALICE O2 Monitoring system
 namespace Core {
-	/// Avaliable modes
-	/// Rate
+	/// Avaliable registration modes
+	/// 1. Rate
 	const int REGISTER_RATE = 1;
-	/// Average value
+	/// 2. Average value
 	const int REGISTER_AVERAGE = 2;
 
 class Collector {
 
 private:
-        ConfigFile mConfigFile;
+	/// Loaded configuration file.
+	ConfigFile mConfigFile;
 
-	/// Vector of backends, values are sent to all of them
+	/// Vector of backends (where the values are send to).
 	std::vector <Backend*> backends;
 
-	/// Cache of registered metrics (metric name / vector of metric pointers)
+	/// Cache of registered metrics (metric name / vector of metric pointers).
         std::map <std::string, std::vector<Metric*>> cache;
 
-	/// Registered metrics with their modes (metric name, registered mode)
+	/// Registered metrics with their modes (metric name, registered mode).
+	/// See list of modes in begiing of the file.
 	std::map <std::string, int> registered;
 public:
+	/// Reads configuration file and based on it loaded backends.
         Collector();
 
-	/// Generates timestamp in microseconds
+	/// Generates timestamp in miliseconds
 	/// \return timestamp as unsigned long
 	static unsigned long getCurrentTimestamp();
 
-	/// Keeps past metric in container and allows to process them to calculate needed values (eg. metric rate, average value)
-	/// Then, it sends the metric to backends
-	/// param value value of the metric
-	/// param name name of the metric
+	/// Stores past metrics in container in order to allow create new metrics based on them
+	/// (eg. metric rate, average value).
+	/// Then, it sends the metric to all of backends (stored in "backends" vector)
+	
+	/// param value of the metric
+	/// param name of the metric
 	/// param entity where the metric come from
-	/// param tiemstamp timestamp in miliseconds; by default output of getCurrentTimestamp is assigned
+	/// param tiemstamp in miliseconds, if not provided output of getCurrentTimestamp as default value is assigned
 	template<typename T> void send(T value, std::string name, std::string entity, unsigned long timestamp = Collector::getCurrentTimestamp());
   
-	/// Calculates rate bansed on values and timestamps of current and previous metric
-	/// not aplicable for strings
-	void injectRate(std::string name);
-
-	/// Register metric to be processed
+	/// Registers metric - to be processed in one of the modes
 	/// Following processing modes are supported: REGISTER_RATE, REGISTER_AVERAGE
 	void registerMetric(std::string name, int mode);
 
@@ -62,7 +63,17 @@ public:
 	/// Inserts metric into std::map cache
 	/// param metric pointer to metric object
 	void insert(Metric* metric);
+
+	/// Calculates rate based on past and curret value and timestamp      
+	/// \param name
+	void injectRate(std::string name);
+
+	/// Calculates average value based on all past values
+	/// param name
 	void injectAverage(std::string name);
+
+	/// Deallocates all the memory (metrics and backends)
+	/// TODO : move to smart pointers
         ~Collector();
 
 };
