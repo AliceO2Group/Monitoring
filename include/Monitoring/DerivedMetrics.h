121328bd8abbe991b84daba3d62e30234c44b734
awegrzyn <adam.wegrzynek@cern.ch>
2016-04-28 15:34:12 +0200

    init Derived Metric class

---
+++ include/Monitoring/DerivedMetrics.h
@@ -0,0 +1,63 @@
+#ifndef ALICEO2_MONITORING_CORE_DERIVED_METRICS_H
+#define ALICEO2_MONITORING_CORE_DERIVED_METRICS_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include "Monitoring/Metric.h"
+
+namespace AliceO2 {
+/// ALICE O2 Monitoring system
+namespace Monitoring {
+/// Core features of ALICE O2 Monitoring system
+namespace Core {
+	/// Avaliable derived metric modes : RATE and AVERAGE values
+	enum class DerivedMetricMode { RATE, AVERAGE };
+
+class DerivedMetrics {
+
+private:
+	
+	/// Cache of registered metrics (metric name / vector of metric pointers).
+        std::map <std::string, std::vector<Metric*>> cache;
+
+	/// Registered metrics with their modes (metric name, registered mode).
+	/// See list of modes in begiing of the file.
+	std::map <std::string, DerivedMetricMode> registered;
+
+public:
+        DerivedMetrics()  = default;
+	~DerivedMetrics() = default;
+	/// States whether metric has been registered or not
+	/// \return yes or no
+	bool isRegistered(std::string name);
+
+	/// Registers metric - to be processed in one of the modes
+	/// Following processing modes are supported: REGISTER_RATE, REGISTER_AVERAGE
+	void registerMetric(DerivedMetricMode mode, std::string name);
+
+	/// Handles metric processing, switches over processing modes
+	/// \return derived metric
+	Metric* processMetric(Metric* metric);
+
+	/// Inserts metric into std::map cache
+	/// param metric pointer to metric object
+	void insert(Metric* metric);
+
+	/// Calculates rate based on past and curret value and timestamp      
+	/// \param name
+	/// \return derived metric
+	Metric* calculateRate(std::string name);
+
+	/// Calculates average value based on all past values
+	/// \param name
+	/// \return derived metric
+	Metric* calculateAverage(std::string name);
+
+};
+
+} // namespace Core
+} // namespace Monitoring
+} // namespace AliceO2
+
+#endif // ALICEO2_MONITORING_CORE_DERIVED_METRICS_H
---
+++ src/DerivedMetrics.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <map>
+#include "Monitoring/DerivedMetrics.h"
+#include "Monitoring/StdoutBackend.h"
+#include "Monitoring/TemplatedMetric.h"
+#include "Monitoring/MonInfoLogger.h"
+
+#ifdef _WITH_APPMON
+#include "Monitoring/ApMonBackend.h"
+#endif
+
+namespace AliceO2 {
+namespace Monitoring {
+namespace Core {
+
+void DerivedMetrics::registerMetric(DerivedMetricMode mode, std::string name)
+{
+        registered.insert(std::pair<std::string, DerivedMetricMode>(name, mode));
+	MonInfoLogger::GetInstance() << "Monitoring : Metric " << name << " added to derived metrics" << AliceO2::InfoLogger::InfoLogger::endm;
+}
+
+bool DerivedMetrics::isRegistered(std::string name)
+{
+	auto search = registered.find(name);
+        return (search != registered.end());
+}
+
+Metric* DerivedMetrics::calculateRate(std::string name)
+{
+	auto search = cache.find(name);
+        if (search != cache.end())
+	{
+		int size = search->second.size();
+		if (size >= 2)
+		{
+			Metric* subtract = search->second[size - 1]->subtract(search->second[size - 2]);
+			return subtract;
+		}
+	}
+	return nullptr;
+}
+
+Metric* DerivedMetrics::calculateAverage(std::string name)
+{
+	auto search = cache.find(name);
+        if (search != cache.end())
+        {
+		Metric* average = search->second.back()->average(search->second);
+		return average;
+	}
+	return nullptr;
+}
+void DerivedMetrics::insert(Metric* metric)
+{
+	auto search = cache.find(metric->getName());
+	if (search == cache.end())
+        {
+        	cache.insert(std::pair<std::string, std::vector<Metric*>>(metric->getName(), std::vector<Metric*>()));
+	}
+	cache[metric->getName()].push_back(metric);
+}
+Metric* DerivedMetrics::processMetric(Metric* metric)
+{
+	insert(metric);
+	auto search = registered.find(metric->getName());
+	
+	if (search->second == DerivedMetricMode::RATE)
+	{
+		return calculateRate(metric->getName());
+	}
+	else if (search->second == DerivedMetricMode::AVERAGE)
+	{
+		return calculateAverage(metric->getName());
+	}
+	else 
+	{
+		MonInfoLogger::GetInstance() << "Monitoring : Processing mode incorrect for metric " << metric->getName() << AliceO2::InfoLogger::InfoLogger::endm;
+		return nullptr;
+	}
+}
+
+} // namespace Core
+} // namespace Monitoring
+} // namespace AliceO2
+
