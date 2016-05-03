8fd7ea534a0ecd28b50ea0c76a2a00a738a99194
awegrzyn <adam.wegrzynek@cern.ch>
2016-05-03 13:52:58 +0200

    Monitoring : removed process ID generator from AppMon backend

--- src/ApMonBackend.cpp
+++ src/ApMonBackend.cpp
@@ -8,7 +8,6 @@ namespace Core {
 
 ApMonBackend::ApMonBackend(const std::string configurationFile)
 {
-	setProcessUniqueId();
 	try {
 		mApMon = new ApMon(const_cast<char *>(configurationFile.c_str()));
 	} catch (std::runtime_error& e) {
@@ -21,16 +20,6 @@ ApMonBackend::~ApMonBackend()
 	delete mApMon;
 }
 
-void ApMonBackend::setProcessUniqueId()
-{
-	char hostname[255];
-	gethostname(hostname, 255);
-	
-	std::ostringstream format;
-        format << hostname << "." << getpid();
-	
-	mProcessUniqueId = format.str();
-}
 inline int ApMonBackend::convertTimestamp(const std::chrono::time_point<std::chrono::system_clock> timestamp)
 {
 	return std::chrono::duration_cast <std::chrono::milliseconds>(
@@ -40,25 +29,24 @@ inline int ApMonBackend::convertTimestamp(const std::chrono::time_point<std::chr
 
 void ApMonBackend::send(const int value, const std::string name, const std::string entity, const std::chrono::time_point<std::chrono::system_clock> timestamp)
 {
-	mApMon->sendTimedParameter(const_cast<char *>(mProcessUniqueId.c_str()), const_cast<char *>(entity.c_str()),
+	mApMon->sendTimedParameter(const_cast<char *>(entity.c_str()), const_cast<char *>(entity.c_str()),
 		const_cast<char *>(name.c_str()), XDR_INT32, (char *) &value, convertTimestamp(timestamp));
 }
 void ApMonBackend::send(const double value, const std::string name, const std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp)
 {
-	mApMon->sendTimedParameter(const_cast<char *>(mProcessUniqueId.c_str()), const_cast<char *>(entity.c_str()), 
+	mApMon->sendTimedParameter(const_cast<char *>(entity.c_str()), const_cast<char *>(entity.c_str()), 
 		const_cast<char *>(name.c_str()), XDR_REAL64, (char *) &value, convertTimestamp(timestamp));
 }
 void ApMonBackend::send(const std::string value, const std::string name, const std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp)
 {
-	mApMon->sendTimedParameter(const_cast<char *>(mProcessUniqueId.c_str()), const_cast<char *>(entity.c_str()),
+	mApMon->sendTimedParameter(const_cast<char *>(entity.c_str()), const_cast<char *>(entity.c_str()),
 		const_cast<char *>(name.c_str()), XDR_STRING, const_cast<char *>(value.c_str()), convertTimestamp(timestamp));
 }
 void ApMonBackend::send(const uint32_t value, const std::string name, const std::string entity, const std::chrono::time_point<std::chrono::system_clock> timestamp)
 {
-        mApMon->sendTimedParameter(const_cast<char *>(mProcessUniqueId.c_str()), const_cast<char *>(entity.c_str()),
+        mApMon->sendTimedParameter(const_cast<char *>(entity.c_str()), const_cast<char *>(entity.c_str()),
                 const_cast<char *>(name.c_str()), XDR_INT32, (char *) &value, convertTimestamp(timestamp));
 }
 } // namespace Core
 } // namespace Monitoring
 } // namespace AliceO2
-
