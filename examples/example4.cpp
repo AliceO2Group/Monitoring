9d4b865e2400383c552ad4209ebf142d1f894b24
awegrzyn <adam.wegrzynek@cern.ch>
2016-05-03 17:19:23 +0200

    Monitoring : fixing doc

--- examples/example4.cpp
+++ examples/example4.cpp
@@ -9,12 +9,12 @@ int main() {
 	// pass filepath to ApMon configuration as argument
 	Monitoring::Core::Collector *collector = new Monitoring::Core::Collector("../config/apmon.conf");
 
-	// derived metric :  average value and rate
+	// derived metric :  rate
 	collector->addDerivedMetric(Monitoring::Core::DerivedMetricMode::RATE, "myCrazyMetirc");
-	collector->addDerivedMetric(Monitoring::Core::DerivedMetricMode::AVERAGE, "myCrazyMetirc");
 
 	// now send at least two metrics to see the result
 	collector->send(10, "myCrazyMetric");
 	collector->send(20, "myCrazyMetric");
 	collector->send(30, "myCrazyMetric");
+	collector->send(40, "myCrazyMetric");
 }
