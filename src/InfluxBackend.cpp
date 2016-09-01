9f7f5b28c999a4514995810080d6cb30864fc096
Adam Tadeusz Wegrzynek <adam.wegrzynek@cern.ch>
2016-09-01 17:03:15 +0200

    Corrected indentation

--- src/InfluxBackend.cpp
+++ src/InfluxBackend.cpp
@@ -51,42 +51,42 @@ void InfluxBackend::send(const uint32_t value, const std::string name, const std
 int InfluxBackend::curlWrite(const std::string value, const std::string name, const std::string entity, 
                              const unsigned long timestamp)
 {
-	std::stringstream convert;
-	CURL *curl;
-        CURLcode response;
+  std::stringstream convert;
+  CURL *curl;
+  CURLcode response;
 
-	// preparing post data
-        convert << name << ",entity=" << entity << " value=" << value << " " << timestamp;
-        string post = convert.str();
+  // preparing post data
+  convert << name << ",entity=" << entity << " value=" << value << " " << timestamp;
+  string post = convert.str();
 	
-	// cURL..
-        curl = curl_easy_init();
-        long responseCode;
-        if(curl) {
-                curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-                curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-                curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 10);
-                curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
-                curl_easy_setopt(curl, CURLOPT_POST, 1);
-                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post.c_str());
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long) post.length());
-                response = curl_easy_perform(curl);
-                curl_easy_getinfo(curl,CURLINFO_RESPONSE_CODE, &responseCode);
-                if (response != CURLE_OK) {
-                        MonInfoLogger::GetInstance() << "!!! InfluxDB : cURL error : " << (curl_easy_strerror(response)) 
-                                                     << AliceO2::InfoLogger::InfoLogger::endm;
-			return 2;
-                }
-                if (responseCode != 204) {
-                        MonInfoLogger::GetInstance() << "!!! InfluxDB : cURL response code " + to_string(responseCode) 
-                                                     << AliceO2::InfoLogger::InfoLogger::endm;
-			return 1;
-                }
-		MonInfoLogger::GetInstance() << "InfluxDB : metric " <<  name << ", code " << responseCode 
-                                             << AliceO2::InfoLogger::InfoLogger::endm;
-        }
-	curl_easy_cleanup(curl);
-        return 0;
+  // cURL..
+  curl = curl_easy_init();
+  long responseCode;
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+    curl_easy_setopt(curl, CURLOPT_POST, 1);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post.c_str());
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long) post.length());
+    response = curl_easy_perform(curl);
+    curl_easy_getinfo(curl,CURLINFO_RESPONSE_CODE, &responseCode);
+    if (response != CURLE_OK) {
+      MonInfoLogger::GetInstance() << "!!! InfluxDB : cURL error : " << (curl_easy_strerror(response)) 
+                                   << AliceO2::InfoLogger::InfoLogger::endm;
+      return 2;
+    }
+    if (responseCode != 204) {
+      MonInfoLogger::GetInstance() << "!!! InfluxDB : cURL response code " + to_string(responseCode) 
+                                   << AliceO2::InfoLogger::InfoLogger::endm;
+      return 1;
+    }
+      MonInfoLogger::GetInstance() << "InfluxDB : metric " <<  name << ", code " << responseCode 
+                                   << AliceO2::InfoLogger::InfoLogger::endm;
+    }
+    curl_easy_cleanup(curl);
+    return 0;
 }
 
 } // namespace Core
