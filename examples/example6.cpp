2b3b4f08c5aec6c2cf6bd46fa79b35758cbf6262
Adam Tadeusz Wegrzynek <adam.wegrzynek@cern.ch>
2016-09-05 14:17:56 +0200

    changes in docs as library interfaces changed

--- examples/example6.cpp
+++ examples/example6.cpp
@@ -11,10 +11,12 @@ int main() {
   // create monitoring object and confuguration as parameter to constructor
   std::shared_ptr<Monitoring::Core::Collector> collector(new Monitoring::Core::Collector(configFile));
 
+  // add additional PID to be monitored
   collector->addMonitoredPid(1);
 	
   for (;;) {
     collector->send(10, "mainThreadMetric");
+    // manual monitor update
     collector->monitorUpdate();
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
