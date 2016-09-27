c55f416aa1347792c4c0925fad4aee1e0a47f5a3
Adam Tadeusz Wegrzynek <adam.wegrzynek@cern.ch>
2016-09-27 15:45:21 +0200

    removed spance before *

--- src/ApMonBackend.cxx
+++ src/ApMonBackend.cxx
@@ -14,13 +14,13 @@ namespace AliceO2
 namespace Monitoring 
 {
 /// Core features of ALICE O2 Monitoring system
-//namespace Core
+namespace Core
 {
 
 ApMonBackend::ApMonBackend(const std::string configurationFile)
 {
   try {
-    mApMon = std::unique_ptr<ApMon>(new ApMon(const_cast<char *>(configurationFile.c_str())));
+    mApMon = std::unique_ptr<ApMon>(new ApMon(const_cast<char*>(configurationFile.c_str())));
   } catch (...) {
     MonInfoLogger::GetInstance() << "Could not open ApMon configuration file: " << configurationFile
                                  << AliceO2::InfoLogger::InfoLogger::endm;
@@ -39,26 +39,26 @@ inline int ApMonBackend::convertTimestamp(const std::chrono::time_point<std::chr
 void ApMonBackend::send(int value, const std::string name, const std::string entity, 
                         std::chrono::time_point<std::chrono::system_clock> timestamp) 
 {
-  mApMon->sendTimedParameter(const_cast<char *>(entity.c_str()), const_cast<char *>(entity.c_str()), 
-    const_cast<char *>(name.c_str()), XDR_INT32, reinterpret_cast<char*>(&value), convertTimestamp(timestamp));
+  mApMon->sendTimedParameter(const_cast<char*>(entity.c_str()), const_cast<char*>(entity.c_str()), 
+    const_cast<char*>(name.c_str()), XDR_INT32, reinterpret_cast<char*>(&value), convertTimestamp(timestamp));
 }
 void ApMonBackend::send(double value, const std::string name, const std::string entity, 
                         std::chrono::time_point<std::chrono::system_clock> timestamp)
 {
-  mApMon->sendTimedParameter(const_cast<char *>(entity.c_str()), const_cast<char *>(entity.c_str()), 
-    const_cast<char *>(name.c_str()), XDR_REAL64, reinterpret_cast<char*>(&value), convertTimestamp(timestamp));
+  mApMon->sendTimedParameter(const_cast<char*>(entity.c_str()), const_cast<char*>(entity.c_str()), 
+    const_cast<char*>(name.c_str()), XDR_REAL64, reinterpret_cast<char*>(&value), convertTimestamp(timestamp));
 }
 void ApMonBackend::send(const std::string value, const std::string name, const std::string entity, 
                         std::chrono::time_point<std::chrono::system_clock> timestamp)
 {
-  mApMon->sendTimedParameter(const_cast<char *>(entity.c_str()), const_cast<char *>(entity.c_str()),
-    const_cast<char *>(name.c_str()), XDR_STRING, const_cast<char *>(value.c_str()), convertTimestamp(timestamp));
+  mApMon->sendTimedParameter(const_cast<char*>(entity.c_str()), const_cast<char*>(entity.c_str()),
+    const_cast<char*>(name.c_str()), XDR_STRING, const_cast<char*>(value.c_str()), convertTimestamp(timestamp));
 }
 void ApMonBackend::send(uint32_t value, const std::string name, const std::string entity, 
                         const std::chrono::time_point<std::chrono::system_clock> timestamp)
 {
-  mApMon->sendTimedParameter(const_cast<char *>(entity.c_str()), const_cast<char *>(entity.c_str()),
-    const_cast<char *>(name.c_str()), XDR_INT32, reinterpret_cast<char*>(&value), convertTimestamp(timestamp));
+  mApMon->sendTimedParameter(const_cast<char*>(entity.c_str()), const_cast<char*>(entity.c_str()),
+    const_cast<char*>(name.c_str()), XDR_INT32, reinterpret_cast<char*>(&value), convertTimestamp(timestamp));
 }
 } // namespace Core
 } // namespace Monitoring
