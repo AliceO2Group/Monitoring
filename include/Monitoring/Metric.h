3b336e54510bc43a92304885fe893593e0228254
Adam Tadeusz Wegrzynek <adam.wegrzynek@cern.ch>
2016-09-27 13:58:17 +0200

    make getters const

--- include/Monitoring/Metric.h
+++ include/Monitoring/Metric.h
@@ -44,33 +44,33 @@ public:
   /// \param value of the metric
   /// \param name of the metric
   /// \param timestamp in miliseconds, if not provided output of getCurrentTimestamp as default value is assigned
-  Metric(int value, std::string name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp);
-  Metric(std::string value, std::string name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp);
-  Metric(double value, std::string name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp);
-  Metric(uint32_t value, std::string name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp);
+  Metric(int value, const std::string& name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp);
+  Metric(std::string value, const std::string& name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp);
+  Metric(double value, const std::string& name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp);
+  Metric(uint32_t value, const std::string& name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp);
 	
   /// Default destructor
   ~Metric() = default;
         
   /// Name getter
   /// \return	metric name
-  std::string getName();
+  std::string getName() const;
 
   /// Timestamp getter
   /// \return 	metric timestamp
-  std::chrono::time_point<std::chrono::system_clock> getTimestamp();
+  std::chrono::time_point<std::chrono::system_clock> getTimestamp() const;
 	
   /// Value getter
   /// \return metric value
-  boost::variant< int, std::string, double, uint32_t > getValue();
+  boost::variant< int, std::string, double, uint32_t > getValue() const;
 
   /// Entity getter
   /// \return metric entity
-  std::string getEntity();
+  std::string getEntity() const;
 
   /// Value type getter
   /// \return type of value stores inside metric : 0 - int, 1 - std::string, 2 - double, 3 - uint32_t
-  int getType();
+  int getType() const;
 };
 
 } // namespace Core
--- src/Metric.cxx
+++ src/Metric.cxx
@@ -20,43 +20,43 @@ namespace Monitoring
 namespace Core
 {
 
-std::string Metric::getEntity()
+std::string Metric::getEntity() const
 {
   return mEntity;
 }
 
-std::chrono::time_point<std::chrono::system_clock> Metric::getTimestamp()
+std::chrono::time_point<std::chrono::system_clock> Metric::getTimestamp() const
 {
   return mTimestamp;
 }
 
-int Metric::getType()
+int Metric::getType() const
 {
   return mValue.which();
 }
 
-std::string Metric::getName()
+std::string Metric::getName() const
 {
   return mName;
 }
 
-Metric::Metric(int value, std::string name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp) :
+Metric::Metric(int value, const std::string& name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp) :
   mValue(value), mName(name), mEntity(entity), mTimestamp(timestamp)
 {}
 
-Metric::Metric(std::string value, std::string name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp) :
+Metric::Metric(std::string value, const std::string& name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp) :
   mValue(value), mName(name), mEntity(entity), mTimestamp(timestamp)
 {}
 
-Metric::Metric(double value, std::string name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp) :
+Metric::Metric(double value, const std::string& name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp) :
   mValue(value), mName(name), mEntity(entity), mTimestamp(timestamp)
 {}
 
-Metric::Metric(uint32_t value, std::string name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp) :
+Metric::Metric(uint32_t value, const std::string& name, std::string entity, std::chrono::time_point<std::chrono::system_clock> timestamp) :
   mValue(value), mName(name), mEntity(entity), mTimestamp(timestamp)
 {}
 
-boost::variant< int, std::string, double, uint32_t > Metric::getValue()
+boost::variant< int, std::string, double, uint32_t > Metric::getValue() const
 {
   return mValue;
 }
