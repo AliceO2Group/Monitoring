b7f8d1b4f96cbc53e01dd8557d9e21f8e84647ab
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-06-14 13:57:47 +0200

    Benchmark changed for monitroing tests

--- examples/5-Benchmark.cxx
+++ examples/5-Benchmark.cxx
@@ -3,55 +3,54 @@
 /// \author Adam Wegrzynek <adam.wegrzynek@cern.ch>
 ///
 
-#include <boost/program_options.hpp>
 #include "Monitoring/MonitoringFactory.h"
+#include <boost/program_options.hpp>
+#include <random>
 
 using Monitoring = AliceO2::Monitoring::MonitoringFactory;
 
 int main(int argc, char *argv[]) {
   int sleep = 1000000;
-  int i = std::numeric_limits<int>::max() - 1;
-  std::string metric = "myCrazyMetric";
-  std::string config = "/home/awegrzyn/hackathon/Monitoring/examples/SampleConfig.ini";
+  std::string stringMetric = "stringMetric";
+  std::string doubleMetric = "doubleMetric";
+  std::string intMetric = "intMetric";
+
+  std::random_device rd;
+  std::mt19937 mt(rd());
+
+  std::uniform_real_distribution<double> doubleDist(1.0, 100.0);
+  std::uniform_int_distribution<> intDist(1, 100);
+
+  double doubleValue = doubleDist(mt);
+  std::string stringValue = "sampleString" + std::to_string(intDist(mt));
+  int intValue = intDist(mt);
 
   boost::program_options::options_description desc("Allowed options");
   desc.add_options()
-    ("metric", boost::program_options::value<std::string>(), "Metric name")
     ("sleep", boost::program_options::value<int>(), "Thread sleep in microseconds")
-    ("count", boost::program_options::value<int>(), "Number of metrics to be sent")
-    ("config", boost::program_options::value<std::string>(), "Config file path")
+    ("config", boost::program_options::value<std::string>()->required(), "Config file path")
   ;
   
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
 
-  if (vm.count("metric")) {
-    metric = vm["metric"].as<std::string>();
-  }
-
   if (vm.count("sleep")) {
     sleep = vm["sleep"].as<int>();
   }
 
-  if (vm.count("count")) {
-    i = vm["count"].as<int>();
-  }
-
-  if (vm.count("config")) {
-    config = vm["config"].as<std::string>();
-  }
-
   try {
     // configure monitoring (once per process), pass configuration path as parameter
-    Monitoring::Configure("file://" + config);
+    Monitoring::Configure("file://" + vm["config"].as<std::string>());
   } catch (std::string &e) {
-    std::cout << "Run you examples from 'build' (dev) or 'bin' (install) direcotry\n";
-    std::cout << e << std::endl;
+    std::cout << "Configuration file not found.\n" << e << std::endl;
+    std::exit(EXIT_FAILURE);
   }  
 
-  for (; i > 0; i--) {
-    Monitoring::Get().send(10, metric);
+  for (;;) {
+    Monitoring::Get().send(stringValue, stringMetric);
+    Monitoring::Get().send(doubleValue, doubleMetric);
+    Monitoring::Get().send(intValue, intMetric);
     std::this_thread::sleep_for(std::chrono::microseconds(sleep));
   }
 }	
