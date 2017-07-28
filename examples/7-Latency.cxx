97467eb3ce43d4dc7fedb1e9718712cec9c9d084
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-07-28 11:03:44 +0200

    latency sleep added

--- examples/7-Latency.cxx
+++ examples/7-Latency.cxx
@@ -12,14 +12,38 @@ using Monitoring = AliceO2::Monitoring::MonitoringFactory;
 using namespace AliceO2::Monitoring;
 
 int main(int argc, char *argv[]) {
-  Monitoring::Configure("file://" + GetConfigFromCmdLine(argc, argv));
+  int sleep = 10000;
+  boost::program_options::options_description desc("Allowed options");
+  desc.add_options()
+    ("sleep", boost::program_options::value<int>(), "Thread sleep in microseconds")
+    ("config", boost::program_options::value<std::string>()->required(), "Config file path")
+  ;
+
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+  boost::program_options::notify(vm);
+
+  if (vm.count("sleep")) {
+    sleep = vm["sleep"].as<int>();
+  }
+
+  try {
+    Monitoring::Configure("file://" + vm["config"].as<std::string>());
+  } catch (std::string &e) {
+    std::cout << "Configuration file not found.\n" << e << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
   auto timestamp = std::chrono::system_clock::now();
   
-  Monitoring::Get().send(
-    Metric{999, "latencyTestMetric"}.addTags(
-      {{"libraryTimestamp", std::to_string(
-        std::chrono::duration_cast <std::chrono::nanoseconds>(timestamp.time_since_epoch()).count()
-      )}} 
-    )
-  );
+  for (;;) {
+    Monitoring::Get().send(
+      Metric{999, "latencyTestMetric"}.addTags(
+        {{"libraryTimestamp", std::to_string(
+          std::chrono::duration_cast <std::chrono::nanoseconds>(timestamp.time_since_epoch()).count()
+        )}}
+      )
+    );
+    std::this_thread::sleep_for(std::chrono::microseconds(sleep));
+  }
 }	
