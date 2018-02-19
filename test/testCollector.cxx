6ef7344801457065315f8addf27fd6f21a9e28f4
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-02-19 11:36:05 +0100

    testing increment

--- test/testCollector.cxx
+++ test/testCollector.cxx
@@ -1,10 +1,13 @@
-#include "../include/Monitoring/MonitoringFactory.h"
 #include <chrono>
 #include <vector>
 
 #define BOOST_TEST_MODULE Test Monitoring Colletor
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+#include <boost/lexical_cast.hpp>
+
+#define private public // evil hack to test private method
+#include "../include/Monitoring/MonitoringFactory.h"
 
 namespace AliceO2 {
 namespace Monitoring {
@@ -23,6 +26,18 @@ BOOST_AUTO_TEST_CASE(createCollector)
   collector->send(intMetric, "myCrazyMetricI");
   collector->send(stringMetric, "myCrazyMetricS");
   collector->send(doubleMetric, "myCrazyMetricD");
+}
+
+BOOST_AUTO_TEST_CASE(testIncrement)
+{
+  int value = 5;
+  double dValue = 5.1;
+  auto collector = Monitoring::Get("infologger://");
+  BOOST_CHECK_EQUAL(boost::lexical_cast<int>((collector->incrementMetric(value, "test")).getValue()), value);
+  BOOST_CHECK_EQUAL(boost::lexical_cast<int>((collector->incrementMetric(value, "test")).getValue()), value*2);
+  BOOST_CHECK_EQUAL(boost::lexical_cast<double>((collector->incrementMetric(dValue, "test2")).getValue()), dValue);
+  BOOST_CHECK_EQUAL(boost::lexical_cast<double>((collector->incrementMetric(dValue, "test2")).getValue()), dValue*2);
+  BOOST_CHECK_EQUAL(boost::lexical_cast<int>((collector->incrementMetric(value, "test")).getValue()), value*3);
 }
 
 BOOST_AUTO_TEST_CASE(testSymbols)
