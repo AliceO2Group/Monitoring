808a6189881e287462f41f2800055b43882f0010
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2018-06-29 13:56:47 +0100

    Improve process monitoring (#58)
    Read /proc file instead of using bash command when getting network stats

--- src/ProcessMonitor.cxx
+++ src/ProcessMonitor.cxx
@@ -10,6 +10,8 @@
 #include <chrono>
 #include <sstream>
 #include <cmath>
+#include <fstream>
+#include <utility>
 
 namespace o2
 {
@@ -22,30 +24,6 @@ ProcessMonitor::ProcessMonitor()
   mPid = static_cast<unsigned int>(::getpid());
   getrusage(RUSAGE_SELF, &mPreviousGetrUsage);
   mTimeLastRun = std::chrono::high_resolution_clock::now();
-}
-
-std::vector<Metric> ProcessMonitor::getNetworkUsage()
-{
-  std::vector<Metric> metrics;
-  std::stringstream ss;
-  // get bytes received and transmitted per interface
-  ss << "cat /proc/" << mPid << "/net/dev | tail -n +3 | grep -v -e 'lo' -e 'virbr0' | awk ' {print $1 $2 \":\" $10}'";
-  std::string output = exec(ss.str().c_str());
-  // for each line (each network interfrace)
-  std::istringstream iss(output);
-  for (std::string line; std::getline(iss, line); ) {
-    auto position = line.find(":");
-    auto secondPosition = line.find(":", position + 1);
-    metrics.emplace_back(Metric{
-      static_cast<uint64_t>(std::stoull(line.substr(position + 1, secondPosition - position - 1))),
-      "bytesReceived"}.addTags({{"if", line.substr(0, position)}})
-    );
-    metrics.emplace_back(Metric{
-      static_cast<uint64_t>(std::stoull(line.substr(secondPosition + 1, line.size()))),
-      "bytesTransmitted"}.addTags({{"if", line.substr(0, position)}})
-    );
-  }
-  return metrics;
 }
 
 Metric ProcessMonitor::getMemoryUsage()
@@ -80,6 +58,31 @@ std::vector<Metric> ProcessMonitor::getCpuAndContexts() {
   mTimeLastRun = timeNow;
   mPreviousGetrUsage = currentUsage;
   return metrics;
+}
+
+std::vector<Metric> ProcessMonitor::getNetworkUsage()
+{
+  std::vector<Metric> metrics;
+  std::ifstream infile("/proc/" + std::to_string(mPid) + "/net/dev");
+  std::string line;
+  std::getline(infile, line);
+  std::getline(infile, line);
+  while (std::getline(infile, line)) {
+    if (line.find("virbr") != std::string::npos) continue;
+    if (line.find("lo") != std::string::npos) continue;
+    std::istringstream iss(line);
+    std::vector<std::string> tokens{std::istream_iterator<std::string>{iss},
+                      std::istream_iterator<std::string>{}};
+    metrics.emplace_back(Metric{
+      static_cast<uint64_t>(std::stoull(tokens[1])),
+      "bytesReceived"}.addTags({{"if", tokens[0]}})
+    );
+    metrics.emplace_back(Metric{
+      static_cast<uint64_t>(std::stoull(tokens[9])),
+      "bytesTransmitted"}.addTags({{"if", tokens[0]}})
+    );
+  }
+return metrics;
 }
 
 std::string ProcessMonitor::exec(const char* cmd)
