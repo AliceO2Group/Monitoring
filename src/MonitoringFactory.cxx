babfc52a65d9879eacc25aafb4fe2a5a911cfd6c
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-09-26 15:10:10 +0200

    Rename constat thats overwritten by ApMons define

--- src/MonitoringFactory.cxx
+++ src/MonitoringFactory.cxx
@@ -70,8 +70,8 @@ std::unique_ptr<Backend> getFlume(http::url uri) {
 
 void MonitoringFactory::SetVerbosity(std::string selected, std::unique_ptr<Backend>& backend) {
   static const std::map<std::string, backend::Verbosity> verbosities = {
-    {"/prod", backend::Verbosity::PROD},
-    {"/debug", backend::Verbosity::DEBUG}
+    {"/prod", backend::Verbosity::Prod},
+    {"/debug", backend::Verbosity::Debug}
   };
 
   auto found = verbosities.find(selected);
