c9bddd46b4602be95ce47f8aa2b8799e056cde4d
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2019-01-23 14:32:39 +0100

    Add verbosity example

---
+++ examples/3-Verbosity.cxx
@@ -0,0 +1,27 @@
+///
+/// \file 1-Basic.cxx
+/// \author Adam Wegrzynek <adam.wegrzynek@cern.ch>
+///
+
+#include "Monitoring/MonitoringFactory.h"
+
+using namespace o2::monitoring;
+
+int main() {
+  // Configure monitoring
+  // Pass string with list of URLs as parameter
+  auto monitoring = MonitoringFactory::Get("stdout:///info");
+  
+  // now send an application specific metric
+  // 10 is the value
+  // myMetric is the name of the metric by creating and moving Metric object
+  monitoring->send({10, "myMetricInt", Verbosity::DEBUG});
+  monitoring->send({10.10, "myMetricFloat", Verbosity::PROD});
+
+  monitoring->sendGrouped("measurementName", {{20, "myMetricIntMultiple"}, {20.30, "myMetricFloatMultple"}}, Verbosity::DEBUG);
+  monitoring->sendGrouped("measurementName", {{20, "myMetricIntMultiple"}, {20.30, "myMetricFloatMultple"}}, Verbosity::PROD);
+
+  monitoring->enableBuffering();
+  monitoring->send({10, "myMetricInt", Verbosity::DEBUG});
+  monitoring->send({10.10, "myMetricFloat", Verbosity::PROD});
+}	
