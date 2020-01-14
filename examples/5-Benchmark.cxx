729fed6aa239fdf3e8a42b58baccec203492fc2d
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2020-01-14 18:22:55 +0100

    Extend monitoring benchmark with FLP tags

--- examples/5-Benchmark.cxx
+++ examples/5-Benchmark.cxx
@@ -7,14 +7,15 @@
 #include <boost/program_options.hpp>
 #include <random>
 
-using Monitoring = o2::monitoring::MonitoringFactory;
 using o2::monitoring::Metric;
+using namespace o2::monitoring;
 
 int main(int argc, char* argv[])
 {
   int sleep = 1000000;
   int count = 1;
   int measurements = 1;
+  int flps = 1;
 
   std::srand(std::time(nullptr));
 
@@ -25,11 +26,15 @@ int main(int argc, char* argv[])
   std::uniform_int_distribution<> intDist(1, 100);
 
   boost::program_options::options_description desc("Allowed options");
-  desc.add_options()("sleep", boost::program_options::value<int>(), "Thread sleep in microseconds")("url", boost::program_options::value<std::string>()->required(), "URL to monitoring backend (or list of comma seperated URLs)")("id", boost::program_options::value<std::string>(), "Instance ID")("count", boost::program_options::value<int>(), "Number of loop cycles")("multiple", boost::program_options::bool_switch()->default_value(false), "Sends multiple metrics per measurement")("latency", boost::program_options::bool_switch()->default_value(false), "Sends timestamp as a value")("monitor", boost::program_options::bool_switch()->default_value(false), "Enabled process monitor")("buffer", boost::program_options::value<int>(), "Creates buffr of given size")("measurements", boost::program_options::value<int>(), "Number of different measurements");
+  desc.add_options()("sleep", boost::program_options::value<int>(), "Thread sleep in microseconds")("url", boost::program_options::value<std::string>()->required(), "URL to monitoring backend (or list of comma seperated URLs)")("id", boost::program_options::value<std::string>(), "Instance ID")("count", boost::program_options::value<int>(), "Number of loop cycles")("multiple", boost::program_options::bool_switch()->default_value(false), "Sends multiple metrics per measurement")("latency", boost::program_options::bool_switch()->default_value(false), "Sends timestamp as a value")("monitor", boost::program_options::bool_switch()->default_value(false), "Enabled process monitor")("buffer", boost::program_options::value<int>(), "Creates buffr of given size")("measurements", boost::program_options::value<int>(), "Number of different measurements")("flps", boost::program_options::value<int>(), "Number of FLPs (tags)");
 
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
+
+  if (vm.count("flps")) {
+    flps = vm["flps"].as<int>();
+  }
 
   if (vm.count("sleep")) {
     sleep = vm["sleep"].as<int>();
@@ -43,7 +48,7 @@ int main(int argc, char* argv[])
     measurements = vm["measurements"].as<int>();
   }
 
-  auto monitoring = Monitoring::Get(vm["url"].as<std::string>());
+  auto monitoring = MonitoringFactory::Get(vm["url"].as<std::string>());
   if (vm["monitor"].as<bool>()) {
     monitoring->enableProcessMonitoring(1);
   }
@@ -76,9 +81,12 @@ int main(int argc, char* argv[])
     }
     for (int j = 1; j <= count; j++) {
       for (int i = 1; i <= measurements; i++) {
-        monitoring->send({doubleDist(mt), "doubleMetric" + std::to_string(i)});
-        monitoring->send({intDist(mt), "intMetric" + std::to_string(i)});
-        monitoring->send({std::rand() % 2, "onOffMetric" + std::to_string(i)});
+        for (int k = 1; k <= flps; k++) {
+          monitoring->send(Metric{doubleDist(mt), "doubleMetric" + std::to_string(i)}.addTag(tags::Key::FLP, k));
+          monitoring->send(Metric{intDist(mt), "intMetric" + std::to_string(i)}.addTag(tags::Key::FLP, k));
+          monitoring->send(Metric{std::rand() % 2, "onOffMetric" + std::to_string(i)}.addTag(tags::Key::FLP, k));
+          std::this_thread::sleep_for(std::chrono::microseconds(10));
+        }
         std::this_thread::sleep_for(std::chrono::microseconds(sleep));
       }
       if (!vm.count("count"))
