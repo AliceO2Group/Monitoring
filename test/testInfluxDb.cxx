1d5d362d4f53e4cf233dd7c0cd752d600b5c699f
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2020-04-28 11:45:41 +0200

    Support ROC tools

--- test/testInfluxDb.cxx
+++ test/testInfluxDb.cxx
@@ -28,8 +28,31 @@ BOOST_AUTO_TEST_CASE(simplySendMetric)
 
 BOOST_AUTO_TEST_CASE(simplySendMetric2)
 {
+  using std::string_literals::operator""s;
   auto monitoring = MonitoringFactory::Get("influxdb-stdout://");
-  monitoring->send(Metric{10, "myCrazyMetric"});
+  monitoring->send(Metric{"card"}
+    .addValue(40.217773, "temperature")
+    .addValue(0, "droppedPackets")
+    .addValue(0.768170, "ctpClock")
+    .addValue(240.471130, "localClock")
+    .addValue(0, "totalPacketsPerSecond")
+    .addTag(tags::Key::ID, 3)
+    .addTag(tags::Key::Type, tags::Value::CRU)
+  );
+
+  monitoring->send(Metric{"link"}
+    .addValue("GBT/GBT"s, "gbtMode")
+    .addValue("None"s, "loopback")
+    .addValue("TTC:CTP"s, "gbtMux")
+    .addValue("Continuous"s, "datapathMode")
+    .addValue("Disabled"s,  "datapath")
+    .addValue(181.370575, "rxFreq")
+    .addValue(196.250259, "txFreq")
+    .addValue(0, "status")
+    .addValue(657.400024, "opticalPower")
+    .addTag(tags::Key::CRU, 3)
+    .addTag(tags::Key::ID, 3)
+  );
 }
 
 } // namespace Test
