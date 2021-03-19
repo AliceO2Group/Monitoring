01d35c853e88bd8f481a99c675dcf5ff074196de
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2021-03-19 16:27:23 +0100

    Fix Apmon backend (#236)

--- src/Backends/ApMonBackend.cxx
+++ src/Backends/ApMonBackend.cxx
@@ -68,7 +68,7 @@ void ApMonBackend::send(const Metric& metric)
     entity += '=';
     (value > 0) ? entity += tags::GetValue(value) : entity += std::to_string(0 - value);
   }
-  if (mRunNumber != 0) convert << ",run=" << mRunNumber;
+  if (mRunNumber != 0) entity += (",run=" + std::to_string(mRunNumber));
 
   int valueSize = metric.getValuesSize();
   char **paramNames, **paramValues;
