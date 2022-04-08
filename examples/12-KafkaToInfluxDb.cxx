ed0bd72851edef737688947d55874691c33fded2
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2022-04-08 15:47:47 +0200

    Add final example

--- examples/12-KafkaToInfluxDb.cxx
+++ examples/12-KafkaToInfluxDb.cxx
@@ -22,22 +22,23 @@ int main(int argc, char* argv[])
   boost::program_options::options_description desc("Program options");
   desc.add_options()
     ("kafka-host", boost::program_options::value<std::string>()->required(), "Kafka broker hostname")
-    ("kafka-topic", boost::program_options::value<std::string>()->required(), "Kafka topic")
-    ("grafana-host", boost::program_options::value<std::string>()->required(), "Grafana hostname")
-    ("grafana-key", boost::program_options::value<std::string>()->required(), "Grafana API key");
+    ("kafka-topic", boost::program_options::value<std::string>()->required(), "Kafka topic");
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
 
   auto kafkaConsumer = std::make_unique<transports::KafkaConsumer>(vm["kafka-host"].as<std::string>(), 9092, vm["kafka-topic"].as<std::string>());
-  aliceo2::envs::ActiveRunsList runsMessage;
   for (;;) {
-    auto metrics = kafkaConsumer->receive();
-    if (!metrics.empty()) {
-      for (auto& metric : metrics) {
-        std::cout << metric << std::endl;
+    auto changes = kafkaConsumer->receive();
+    if (!changes.empty()) {
+      for (auto& change : changes) {
+        aliceo2::envs::NewStateNotification stateChange;
+        stateChange.ParseFromString(change);
+        std::cout << "EnvID: " << stateChange.envinfo().environmentid() << std::endl
+                  << "State: " << stateChange.envinfo().state() << std::endl
+                  << "First detector: " << stateChange.envinfo().detectors().Get(0) << std::endl;
       }
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 }
