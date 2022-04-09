fe68e483905b00ff79cf8ea504090111afcd6a63
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2022-04-09 20:18:05 +0200

    [OMON-539] Push values to InfluxDB in example 12

--- examples/12-KafkaToInfluxDb.cxx
+++ examples/12-KafkaToInfluxDb.cxx
@@ -4,8 +4,8 @@
 ///
 
 #include "../src/Transports/KafkaConsumer.h"
+#include "../src/Backends/InfluxDB.h"
 #include "../src/Transports/HTTP.h"
-#include "../src/Transports/WebSocket.h"
 
 #include <iostream>
 #include <memory>
@@ -22,18 +22,31 @@ int main(int argc, char* argv[])
   boost::program_options::options_description desc("Program options");
   desc.add_options()
     ("kafka-host", boost::program_options::value<std::string>()->required(), "Kafka broker hostname")
-    ("kafka-topic", boost::program_options::value<std::string>()->required(), "Kafka topic");
+    ("kafka-topic", boost::program_options::value<std::string>()->required(), "Kafka topic")
+    ("influxdb-host", boost::program_options::value<std::string>()->required(), "InfluxDB hostname")
+    ("influxdb-token", boost::program_options::value<std::string>()->required(), "InfluxDB token")
+    ("influxdb-org", boost::program_options::value<std::string>()->default_value("cern"), "InfluxDB organisation")
+    ("influxdb-bucket", boost::program_options::value<std::string>()->default_value("aliecs"), "InfluxDB bucket");
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
 
   auto kafkaConsumer = std::make_unique<transports::KafkaConsumer>(vm["kafka-host"].as<std::string>(), 9092, vm["kafka-topic"].as<std::string>());
+  auto httpTransport = std::make_unique<transports::HTTP>(
+    "http://" + vm["influxdb-host"].as<std::string>() + ":8086/api/v2/write?" +
+    "org=" + vm["influxdb-org"].as<std::string>() + "&" +
+    "bucket=" + vm["influxdb-bucket"].as<std::string>()
+  );
+  httpTransport->addHeader("Authorization: Token " + vm["influxdb-token"].as<std::string>());
+  auto influxdbBackend = std::make_unique<backends::InfluxDB>(std::move(httpTransport));
+  influxdbBackend->addGlobalTag("state", vm["kafka-topic"].as<std::string>());
   for (;;) {
     auto changes = kafkaConsumer->receive();
     if (!changes.empty()) {
       for (auto& change : changes) {
         aliceo2::envs::NewStateNotification stateChange;
         stateChange.ParseFromString(change);
+        influxdbBackend->send(Metric{stateChange.timestamp(), "state_change"});
         std::cout << "EnvID: " << stateChange.envinfo().environmentid() << std::endl
                   << "State: " << stateChange.envinfo().state() << std::endl
                   << "First detector: " << stateChange.envinfo().detectors().Get(0) << std::endl;
