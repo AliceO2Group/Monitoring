398262e89c2e012902e3cd13f2d3aea7016282a4
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2022-04-13 18:57:52 +0200

    Move to lambdas

--- examples/8-KafkaToHttpServer.cxx
+++ examples/8-KafkaToHttpServer.cxx
@@ -1,3 +1,5 @@
+#define BOOST_BEAST_USE_STD_STRING_VIEW
+
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
@@ -14,8 +16,6 @@
 #include "../src/Transports/KafkaConsumer.h"
 #include "../src/MonLogger.h"
 #include "envs.pb.h"
-
-#define BOOST_BEAST_USE_STD_STRING_VIEW
 
 namespace beast = boost::beast;
 namespace http = beast::http;
@@ -35,8 +35,12 @@ public:
 
   // Initiate the asynchronous operations associated with the connection.
   void start() {
-    read_request();
-    check_deadline();
+    readRequest();
+    checkDeadline();
+  }
+
+  void addCallback(std::string_view path, std::function<void(http::request<http::dynamic_body>& request, http::response<http::dynamic_body>& response)> callback) {
+    mCallbacks.insert({path, callback});
   }
 
 private:
@@ -44,10 +48,11 @@ private:
   beast::flat_buffer mBuffer{8192};
   http::request<http::dynamic_body> mRequest;
   http::response<http::dynamic_body> mResponse;
-  boost::asio::steady_timer deadline_{mSocket.get_executor(), std::chrono::seconds(5)};
+  boost::asio::steady_timer mDeadline{mSocket.get_executor(), std::chrono::seconds(5)};
+  std::map<std::string_view, std::function<void(http::request<http::dynamic_body>& request, http::response<http::dynamic_body>& response)>> mCallbacks;
 
   // Asynchronously receive a complete request message.
-  void read_request() {
+  void readRequest() {
     auto self = shared_from_this();
     http::async_read(mSocket, mBuffer, mRequest, [self](beast::error_code ec, std::size_t bytes_transferred) {
       boost::ignore_unused(bytes_transferred);
@@ -66,32 +71,71 @@ private:
 
   // Construct a response message based on the program state.
   void createResponse() {
-    std::string jsonPrefix = R"({"results": [{"statement_id": 0, "series": [{"name": "env_active", "columns": ["key", "value"], "values": [)";
-    std::string jsonSuffix = R"(]}]}]})";
-    // Handles ""
-    if (mRequest.target().find("SHOW+TAG+VALUES+FROM") != std::string::npos) {
-      mResponse.set(http::field::content_type, "application/json");
-      const std::lock_guard<std::mutex> lock(gEnvAccess);
-      std::string envsJson;
-      for (int i = 0; i < gActiveEnvs.activeruns_size(); i++) {
-        envsJson += "[\"run\", \"" + std::to_string(gActiveEnvs.activeruns(i).runnumber()) + "\"],";
+    for (const auto& [key, value] : mCallbacks) {
+      if (mRequest.target().find(key) != std::string_view::npos) {
+        value(mRequest, mResponse);
+        return;
       }
-      if (!envsJson.empty()) {
-        envsJson.pop_back();
+    }
+    mResponse.result(http::status::not_found);
+    mResponse.set(http::field::content_type, "text/plain");
+    beast::ostream(mResponse.body()) << "Not found\r\n";
+  }
+
+  // Asynchronously transmit the response message.
+  void writeResponse() {
+    auto self = shared_from_this();
+    mResponse.content_length(mResponse.body().size());
+    http::async_write(mSocket, mResponse, [self](beast::error_code ec, std::size_t) {
+      self->mSocket.shutdown(tcp::socket::shutdown_send, ec);
+      self->mDeadline.cancel();
+    });
+  }
+
+  // Check whether we have spent enough time on this connection.
+  void checkDeadline() {
+    auto self = shared_from_this();
+    mDeadline.async_wait(
+    [self](beast::error_code ec) {
+      if(!ec) {
+        self->mSocket.close(ec);
       }
-      beast::ostream(mResponse.body()) << jsonPrefix << envsJson << jsonSuffix << '\n';
-    // Handles "SELECT last() FROM active_runs WHERE run = $run"
-    } else if (mRequest.target().find("active_runs+WHERE+run") != std::string::npos) {
+    });
+  }
+};
+
+// "Loop" forever accepting new connections.
+void httpServer(tcp::acceptor& acceptor, tcp::socket& socket) {
+  acceptor.async_accept(socket, [&](beast::error_code ec) {
+    if (!ec) {
+     auto connection = std::make_shared<httpConnection>(std::move(socket));
+     connection->addCallback("SHOW+TAG+VALUES+FROM",
+     [](http::request<http::dynamic_body>& request, http::response<http::dynamic_body>& response) {
+       std::string jsonPrefix = R"({"results": [{"statement_id": 0, "series": [{"name": "env_active", "columns": ["key", "value"], "values": [)";
+       std::string jsonSuffix = R"(]}]}]})";
+       response.set(http::field::content_type, "application/json");
+       const std::lock_guard<std::mutex> lock(gEnvAccess);
+       std::string envsJson;
+       for (int i = 0; i < gActiveEnvs.activeruns_size(); i++) {
+         envsJson += "[\"run\", \"" + std::to_string(gActiveEnvs.activeruns(i).runnumber()) + "\"],";
+       }
+       if (!envsJson.empty()) {
+         envsJson.pop_back();
+       }
+       beast::ostream(response.body()) << jsonPrefix << envsJson << jsonSuffix << '\n';
+     });
+     connection->addCallback("active_runs+WHERE+run",
+     [](http::request<http::dynamic_body>& request, http::response<http::dynamic_body>& response) {
       std::string jsonPrefix = R"({"results":[{"statement_id":0,"series":[{"name":"env","columns":["time","Env ID","Run number","Detectors","State"],"values":[)";
       std::string jsonSuffix = R"(]}]}]})";
-      mResponse.set(http::field::content_type, "application/json");
-      std::string runString = std::string(mRequest.target().substr(mRequest.target().find("WHERE+run+%3D+") + 14));
+      response.set(http::field::content_type, "application/json");
+      std::string runString = std::string(request.target().substr(request.target().find("WHERE+run+%3D+") + 14));
       uint32_t run;
       try {
         run  = static_cast<uint32_t>(std::stoul(runString));
       } catch(...) {
-        mResponse.set(http::field::content_type, "application/json");
-        beast::ostream(mResponse.body()) << "{}\r\n";
+        response.set(http::field::content_type, "application/json");
+        beast::ostream(response.body()) << "{}\r\n";
         return;
       }
       const std::lock_guard<std::mutex> lock(gEnvAccess);
@@ -108,44 +152,10 @@ private:
                  + boost::algorithm::join(detectors, " ") + "\", \""
                  + gActiveEnvs.activeruns(i).state() + "\"]";
       }
-      beast::ostream(mResponse.body()) << jsonPrefix << envsJson << jsonSuffix << '\n';
-      
-    } else if (mRequest.target().find("SHOW+RETENTION+POLICIES") != std::string::npos) {
-      mResponse.set(http::field::content_type, "application/json");
-      beast::ostream(mResponse.body()) << "{}\r\n";
-    } else {
-      mResponse.result(http::status::not_found);
-      mResponse.set(http::field::content_type, "text/plain");
-      beast::ostream(mResponse.body()) << "Not found\r\n";
+      beast::ostream(response.body()) << jsonPrefix << envsJson << jsonSuffix << '\n';
+     });
+     connection->start();
     }
-  }
-
-  // Asynchronously transmit the response message.
-  void writeResponse() {
-    auto self = shared_from_this();
-    mResponse.content_length(mResponse.body().size());
-    http::async_write(mSocket, mResponse, [self](beast::error_code ec, std::size_t) {
-      self->mSocket.shutdown(tcp::socket::shutdown_send, ec);
-      self->deadline_.cancel();
-    });
-  }
-
-  // Check whether we have spent enough time on this connection.
-  void check_deadline() {
-    auto self = shared_from_this();
-    deadline_.async_wait(
-    [self](beast::error_code ec) {
-      if(!ec) {
-        self->mSocket.close(ec);
-      }
-    });
-  }
-};
-
-// "Loop" forever accepting new connections.
-void httpServer(tcp::acceptor& acceptor, tcp::socket& socket) {
-  acceptor.async_accept(socket, [&](beast::error_code ec) {
-    if (!ec) std::make_shared<httpConnection>(std::move(socket))->start();
     httpServer(acceptor, socket);
   });
 }
