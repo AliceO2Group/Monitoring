3987a79a1e6d3fc2c7880d6764e098032f982bbb
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2023-07-12 15:09:05 +0200

    send metric

--- examples/14-OrbitId.cxx
+++ examples/14-OrbitId.cxx
@@ -3,8 +3,8 @@
 /// \author Adam Wegrzynek <adam.wegrzynek@cern.ch>
 ///
 #include "../src/Transports/KafkaConsumer.h"
-#include "../src/Backends/InfluxDB.h"
 #include "../src/Transports/HTTP.h"
+#include "../src/MonLogger.h"
 
 #include <iostream>
 #include <memory>
@@ -35,11 +35,11 @@ int main(int argc, char* argv[])
 {
   boost::program_options::options_description desc("Program options");
   desc.add_options()
-    ("kafka-host", boost::program_options::value<std::string>()->required(), "Kafka broker hostname");
-    //("influxdb-url", boost::program_options::value<std::string>()->required(), "InfluxDB hostname")
-    //("influxdb-token", boost::program_options::value<std::string>()->required(), "InfluxDB token")
-    //("influxdb-org", boost::program_options::value<std::string>()->default_value("cern"), "InfluxDB organisation")
-    //("influxdb-bucket", boost::program_options::value<std::string>()->default_value("aliecs"), "InfluxDB bucket");
+    ("kafka-host", boost::program_options::value<std::string>()->required(), "Kafka broker hostname")
+    ("influxdb-url", boost::program_options::value<std::string>()->required(), "InfluxDB hostname")
+    ("influxdb-token", boost::program_options::value<std::string>()->required(), "InfluxDB token")
+    ("influxdb-org", boost::program_options::value<std::string>()->default_value("cern"), "InfluxDB organisation")
+    ("influxdb-bucket", boost::program_options::value<std::string>()->default_value("telegraf"), "InfluxDB bucket");
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
@@ -47,13 +47,13 @@ int main(int argc, char* argv[])
   std::vector<std::string> topics = {"aliecs.env_list.RUNNING", "cru.link_status"};
   auto kafkaConsumer = std::make_unique<transports::KafkaConsumer>(vm["kafka-host"].as<std::string>() + ":9092", topics, "orbitid");
   /*auto httpTransport = std::make_unique<transports::HTTP>(
-    vm["influxdb-url"].as<std::string>() + "/api/v2/write?" +
+    "http://" + vm["influxdb-url"].as<std::string>() + "/api/v2/write?" +
     "org=" + vm["influxdb-org"].as<std::string>() + "&" +
     "bucket=" + vm["influxdb-bucket"].as<std::string>()
-  );
-  httpTransport->addHeader("Authorization: Token " + vm["influxdb-token"].as<std::string>());
-  auto influxdbBackend = std::make_unique<backends::InfluxDB>(std::move(httpTransport));*/
-  // output metric: orbitIdMismatch,detector=,run=,link=, sorOrbitId=,mismatchedObrbitId=
+  );*/
+  auto httpTransport = std::make_unique<transports::HTTP>("http://pcald24.cern.ch:8086/api/v2/write?org=cern&bucket=telegraf");
+  //httpTransport->addHeader("Authorization: Token " + vm["influxdb-token"].as<std::string>());
+  MonLogger::mLoggerSeverity = Severity::Debug;
   for (;;) {
     auto messages = kafkaConsumer->pull();
     if (!messages.empty()) {
@@ -72,33 +72,44 @@ int main(int argc, char* argv[])
             }
           }
           if (detectorRunMap.empty()) {
-            std::cout << "No ongoing runs" << std::endl;
+            MonLogger::Get() << "No ongoing runs" << MonLogger::End();
             referenceOrbitIdMap.clear();
           }
           for (const auto &p : detectorRunMap) {
-            std::cout << p.first << " belongs to run " <<  p.second << std::endl;
+            MonLogger::Get() << p.first << " belongs to run " <<  p.second << MonLogger::End();
           }
           // if SOR
         // handle link status messages
         } else if (message.first == "cru.link_status") {
           auto detector = getValueFromMetric("detector", message.second);
           auto orbitId = getValueFromMetric("orbitSor", message.second);
+          auto status = getValueFromMetric("status", message.second);
           if (detector.empty() or orbitId.empty()) {
             continue;
           }
+          // if detector is not running
           auto detectorInRun = detectorRunMap.find(detector);
           if (detectorInRun == detectorRunMap.end()) {
             continue;
           }
+          // if link is excluded
+          if (status != "1i") {
+            continue;
+          }
           auto referenceOrbit = referenceOrbitIdMap.find(detectorRunMap.at(detector));
           if (referenceOrbit == referenceOrbitIdMap.end()) {
+            /// wait for trigger
+            if (orbitId == "0i") {
+              continue;
+            }
             referenceOrbitIdMap.insert({detectorRunMap.at(detector), orbitId});
-            std::cout << "Set reference orbitId for run " << detectorRunMap.at(detector) << ": " << orbitId << std::endl;
+            MonLogger::Get() << "Set reference orbitId for run " << detectorRunMap.at(detector) << ": " << orbitId << MonLogger::End();
           }
           auto referenceOrbitId = referenceOrbitIdMap.at(detectorRunMap.at(detector));
           if (orbitId != referenceOrbitId) {
-            std::cout << "Abnormal condition for " << detector << " expected OrbitID: " << referenceOrbitId << " but got: " << orbitId << std::endl;
-            std::cout << message.second << std::endl;
+             MonLogger::Get() << "Abnormal condition for " << detector << "; expected orbitID: " << referenceOrbitId << " but got: " << orbitId << MonLogger::End();
+             std::string outputMetric = message.second.substr(message.second.find(","), message.second.find(" ") - message.second.find(","));
+             //httpTransport->send("orbitIdMismatch" + outputMetric + " actual=" + orbitId + " expected=" + referenceOrbitId);
           }
         }
       }
